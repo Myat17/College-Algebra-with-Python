a99cd93546671efa157b32a91b6861285b33789c
Myat17 <myathsuthin16@gmail.com>
2026-08-04 21:05:26 +0700

    Modify week 2 exercise 3

--- week2_exercise_3.py
+++ week2_exercise_3.py
@@ -3,11 +3,11 @@ import sympy
 from sympy import symbols
 from sympy import *
 
-var('x y')
+x,y = symbols("x y")
 
 # Equation set equal to zero, read to solve
-eq = 2*x + 10*y + 4
+# eq = x**2 - 4
 
-# eq = x**3 - 2*x**2 - 5*x + 6
+eq = x**3 - 2*x**2 - 5*x + 6
 
-sympy.factor(eq)
+print(sympy.factor(eq))
