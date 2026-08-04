a43722a118c6d67bb76f800193d3613cd6476b83
Myat17 <myathsuthin16@gmail.com>
2026-08-04 20:55:42 +0700

    Complete week 2 exercise 2

---
+++ week2_exercise_2.py
@@ -0,0 +1,19 @@
+# solving in other ways
+from sympy import var, Eq, solve
+
+# This creates two symbolic variables
+var('x y')
+
+# First equation set equal to zero, ready to solve
+first_eq = 2*x - y
+
+# Sympy syntax for equation equal to zero, ready to factor
+# Eq means create a mathematical equation
+# So 2*x-y becomes 2*x-y = 0
+eq1 = Eq(first_eq, 0)
+
+# Sympy solve for x
+sol = solve(eq1, x)
+
+# Show factored results
+print(f"x = {sol[0]}")
