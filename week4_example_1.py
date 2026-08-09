a6ec794407579378aec3b3ba70e9a80f3408a80c
Myat17 <myathsuthin16@gmail.com>
2026-08-09 15:11:17 +0700

    Complete 2 examples of week 4

---
+++ week4_example_1.py
@@ -0,0 +1,11 @@
+# Function and a table of (x,y) values
+x = 5
+y = 4*x + 3 # The function
+
+print(f"{x} , {y}")
+
+print("x \t y")
+
+for x in range(11):
+    y = 4*x + 3 # The function
+    print(f"{x}\t{y}")
---
+++ week4_example_2.py
@@ -0,0 +1,7 @@
+# Define a Python Function
+def f(x):
+    y = 4*x + 3
+    return y
+
+for x in range(11):
+    print(x, "\t", f(x))
