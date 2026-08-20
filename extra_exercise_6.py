f0056771de0705b6cd2ba3853df444e943ff88c3
Myat17 <myathsuthin16@gmail.com>
2026-08-20 21:18:39 +0700

    Complete extra exercise 6

---
+++ extra_exercise_6.py
@@ -0,0 +1,9 @@
+# rounding
+a = 0.006538298335
+b = 14588132
+
+# To round the number of decimal places
+print(f"round of {a} to 3 decimal places = {round(a, 3)}") # will return six decimal places
+
+# To round a large number instead of a decimal number
+print(f"round of {b} to nearest million = {round(b, -6)}") # will return six zeros
