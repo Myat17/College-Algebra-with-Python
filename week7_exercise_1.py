314846ca72ead7a72d07c2bfa2c27414df973be4
Myat17 <myathsuthin16@gmail.com>
2026-09-13 16:29:46 +0700

    Finish Week 7 exercise 1

---
+++ week7_exercise_1.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xmin = -10
+xmax = 10
+ymin = -10
+ymax = 10
+
+# Define how many points to plot
+points = 10 * (xmax - xmin) # bigger multiple number, smoother the curve
+
+# Define the array of x values once
+x = np.linspace(xmin, xmax, points)
+
+fig, ax = plt.subplots()
+plt.axis([xmin, xmax, ymin, ymax])
+plt.plot([xmin, xmax], [0, 0], 'k')
+plt.plot([0, 0], [ymin, ymax], 'k')
+
+# line 1
+y1 = x + 6
+plt.plot(x, y1, '-')
+plt.fill_between(x, y1, ymax, facecolor='red')
+
+# line 2
+y2 = x + 3
+plt.plot(x, y2, '-')
+plt.fill_between(x, y2, y1, facecolor= "yellow")
+
+# line 3
+y3 = x - 1
+plt.plot(x, y3)
+plt.fill_between(x, y3, y2, facecolor= "green")
+
+# line 4
+y4 = x - 4
+plt.plot(x, y4)
+plt.fill_between(x, y4, y3, facecolor= "blue")
+
+plt.show()
